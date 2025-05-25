5beaa5bf2cb77e3fbe7748782a8b927861f38c92
Punit870 <punitkejriwal25@gmail.com>
2025-05-25 19:32:35 +0530

    add logger implementation using winston for improved logging

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import 'dotenv/config'
-import express from 'express';
+import express, { urlencoded } from 'express';
+import logger from'./logger.js';
+import morgan from 'morgan';
+
+const morganFormat = ':method :url :status :response-time ms';
+
+
 
 const app=express()
 
 const port=process.env.PORT||3000
 app.use(express.json())
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message) => {
+            const logObject={
+                method: message.split(' ')[0],
+                urlencoded: message.split(' ')[1],
+                status: message.split(' ')[2],  
+                responseTime: message.split(' ')[3]
+            };
+            logger.info(JSON.stringify(logObject));
+        }
+    }
+}));
 
 let teaData=[]
 let nextId=1
